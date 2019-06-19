b8b2643ce6c0e481aba085b42c68980e35509af3
sin_een <sineen.nawatha@mail.huji.ac.il>
2019-06-19 23:47:52 +0300

    added broadcast class  and added fields in the layout xml

---
+++ app/src/main/java/com/example/sin_een/myspecialstalker/OutgoingCallsBroadcastReciever.java
@@ -0,0 +1,13 @@
+package com.example.sin_een.myspecialstalker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OutgoingCallsBroadcastReciever extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
